0253291e45baa48eedcf231722db7a67424a5047
Sumit <sumitcoding7@gmail.com>
2023-10-05 02:14:44 +0530

    improved UI of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,23 @@ const Navbar = () => {
   const [isNavHidden, setIsNavHidden] = useState(false);
   let prevScrollPos = window.scrollY;
 
+  const [click,setClick] = useState(false);
+  const handleClick = () =>{
+    setClick(!click);
+  };
+
+  //menu-icon
+
+
   useEffect(() => {
     const handleScroll = () => {
       let currScrollPos = window.scrollY;
-      if (currScrollPos > prevScrollPos) {
+      let screen_width=screen.width;
+      /*console.log(screen_width);*/
+      if(screen_width<960){
+        setIsNavHidden(false);
+      }
+      else if (currScrollPos > prevScrollPos ) {
         setIsNavHidden(true);
       } else {
         setIsNavHidden(false);
@@ -54,15 +67,15 @@ const Navbar = () => {
     
     {/* Title */}
     <div className="logo">
-      <img width="100" height="100" src="https://img.icons8.com/plasticine/100/literature.png" alt="literature"/>
+      <img width="80" height="80" src="https://img.icons8.com/plasticine/100/literature.png" alt="literature"/>
       <NavLink to={`/`}>Library</NavLink>
     </div>
 
     {/* Nav Menu */}
-      <div className="menu">
+      <div className={click ? "menu active" : "menu"}>
         
         {/* Links */}
-        <div id="menu-links">
+        <div class={click ? "menu-links active" : "menu-links"}>
 
           <Navlink/>
 
@@ -73,9 +86,16 @@ const Navbar = () => {
       </div>
    
     {/* Side Button */}
+    { /*
       <div className="menu-btn">
-        <i className="fa-solid fa-bars" onClick={showMenu}></i>
-      </div>
+      <i className="fa-solid fa-bars" onClick={showMenu}></i>
+    </div>
+    */
+    }
+    <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+      
     </nav>
 
 
