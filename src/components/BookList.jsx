fd36fa527e7f6e305898f242850e534560dc028a
Aditya Kumar <adityapal1409@gmail.com>
2023-10-06 00:38:17 +0530

    styles: improve color & responsiveness & book card size

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,70 +1,105 @@
-import React, {useEffect} from 'react';
-import {useGlobalContext} from "../Context.jsx";
-import '../assets/styles/BookList.css';
-import {Link} from "react-router-dom";
-import loadingGIF from "../assets/gif/output-onlinegiftools.gif"
-import bookCover from "../assets/images/BookCoverunavailable.jpg"
+import React, { useEffect } from "react";
+import { useGlobalContext } from "../Context.jsx";
+import "../assets/styles/BookList.css";
+import { Link } from "react-router-dom";
+import loadingGIF from "../assets/gif/output-onlinegiftools.gif";
+import bookCover from "../assets/images/BookCoverunavailable.jpg";
 
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const BookList = () => {
+	const { books, searchResult, isLoading } = useGlobalContext();
 
-    const {books, searchResult, isLoading} = useGlobalContext();
+	useEffect(() => {}, [isLoading]);
+	return (
+		<>
+			<p className={"booklist-status"}>
+				{" "}
+				{isLoading
+					? `Fetching Data for ${searchResult}...`
+					: `${searchResult}`}
+			</p>
 
-    useEffect(()=>{
-    },[isLoading])
-    return (
-        <>
-            <p className={"booklist-status"}> {isLoading ? `Fetching Data for ${searchResult}...`:`${searchResult}`}</p>
+			<div className={"bookList-container"}>
+				{isLoading ? (
+					<img
+						className={"loading-gif"}
+						src={loadingGIF}
+						alt="Loading Gif"
+					/>
+				) : (
+					books.map((currentBook) => {
+						return (
+							<section
+								className={"book-card"}
+								key={currentBook.id}>
+								<div className={"book-card-img-container"}>
+									<img
+										src={
+											currentBook.cover_id
+												? `https://covers.openlibrary.org/b/id/${currentBook.cover_id}-M.jpg`
+												: bookCover
+										}
+										alt={"Image is not Available"}
+									/>
+								</div>
 
-            <div className={"bookList-container"}>
+								<div className={"book-card-information"}>
+									<h2>
+										<b>{currentBook.title}</b>
+									</h2>
 
-                {isLoading ? ( <img className={"loading-gif"} src={loadingGIF} alt="Loading Gif" /> ):
-                    books.map((currentBook) => {
-                        return (
-                            <section className={"book-card"} key={currentBook.id}>
+									<p>
+										Author:
+										<span>
+											{" "}
+											{currentBook.author
+												? currentBook.author.join(", ")
+												: "N/A"}
+										</span>
+									</p>
+									<p>
+										Language:
+										<span>
+											{" "}
+											{currentBook.language
+												? currentBook.language.join(
+														", "
+												  )
+												: "N/A"}
+										</span>
+									</p>
 
-                                <div className={"book-card-img-container"}>
-                                    <img
-                                        src={
-                                            currentBook.cover_id
-                                                ? `https://covers.openlibrary.org/b/id/${currentBook.cover_id}-M.jpg`
-                                                : bookCover
-                                        }
-                                        alt={'Image is not Available'} />
-                                </div>
+									<p>
+										Pages:
+										<span> {currentBook.pages}</span>
+									</p>
 
-                                <div className={"book-card-information"}>
-                                    <h2 ><b>{currentBook.title}</b></h2>
+									{/*<Link className={"read-link"} to={`https://openlibrary.org/${currentBook.id}  `}>*/}
+									{/*    {currentBook.available?"Read":"Not Available"}*/}
+									{/*</Link>*/}
 
-                                    <p>Author:
-                                        <span> {currentBook.author ? currentBook.author.join(", ") : "N/A"}</span>
-                                    </p>
-                                    <p>Language:
-                                        <span> {currentBook.language ? currentBook.language.join(", ") : "N/A"}</span>
-                                    </p>
-
-                                    <p>Pages:
-                                        <span> {currentBook.pages}</span>
-                                    </p>
-
-                                    {/*<Link className={"read-link"} to={`https://openlibrary.org/${currentBook.id}  `}>*/}
-                                    {/*    {currentBook.available?"Read":"Not Available"}*/}
-                                    {/*</Link>*/}
-
-                                    <Link className={"read-link"} to={currentBook.read_link && currentBook.read_link.length > 0 ? `https://archive.org/details/${currentBook.read_link[0]}/2up?view=theater` : "#"}>
-                                        {currentBook.available ? "Read" : "Not Available"}
-                                    </Link>
-
-                                </div>
-                            </section>
-                        )
-                    })
-                }
-            </div>
-        </>
-    );
+									<Link
+										className={"read-link"}
+										to={
+											currentBook.read_link &&
+											currentBook.read_link.length > 0
+												? `https://archive.org/details/${currentBook.read_link[0]}/2up?view=theater`
+												: "#"
+										}>
+										{currentBook.available
+											? "Read"
+											: "Not Available"}
+									</Link>
+								</div>
+							</section>
+						);
+					})
+				)}
+			</div>
+		</>
+	);
 };
 
 export default BookList;
