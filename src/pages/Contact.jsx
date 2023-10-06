bdfba4e8c86a0b3452c9de2c3b5cba5570d123f2
Robin <robin.bruch@gmx.de>
2023-10-06 20:50:48 +0200

    Created Contact-Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer';
 import BookList from "../components/BookList.jsx";
-
+import img2 from "../assets/images/books.jpg"
+import '../assets/styles/Contact.css';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+
+    console.log(import.meta.env.VITE_EMAIL_SERVICE_ID, `${import.meta.env.REACT_APP_EMAIL_TEMPLATE_ID}`, form.current, `${import.meta.env.REACT_APP_EMAIL_PUBLIC_KEY}`);
+    emailjs.sendForm(`${import.meta.env.VITE_EMAIL_SERVICE_ID}`, `${import.meta.env.VITE_EMAIL_TEMPLATE_ID}`, form.current, `${import.meta.env.VITE_EMAIL_PUBLIC_KEY}`)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset();
+  }
+
   return (
     <>
     <Navbar/>
+      <section className='contact-wrapper'>
+        <div>
+          <h2 className='form-headline'>Contact Us</h2>
+          <form className='form-wrapper' ref={form} onSubmit={sendEmail}>
+          <label htmlFor="user_name" className="label-name">Full Name:</label>
+            <input className="input" type='text' name='user_name' required/>
 
+            <label htmlFor="user_email" className="label-name">Email:</label>
+            <input className="input" type='email' name='user_email' required/>
 
+            <label htmlFor="user_subject" className="label-name">Subject:</label>
+            <input className="input" type='text' name='user_subject' required/>
+
+            <label htmlFor="message" className="label-name">Message:</label>
+            <textarea name='message' cols="30" rows="10"></textarea>
+
+            <button className="submit-btn" type='submit'>Send Message</button>
+          </form>
+        </div>
+          <img className="contact-img" src={img2} alt='Picture of some books'/>
+      </section>
     <Footer/>
     </>
   )
