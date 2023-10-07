0fa61aadc9eaec7d92a6eab4f3e970bb41c014a5
Sanket Mane <111991599+Sanketmane2323@users.noreply.github.com>
2023-10-07 12:11:25 +0530

    Footer ui changed according the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html" , "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
 
