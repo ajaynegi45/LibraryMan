c5d60370a547e154e0015a8d2a5853c175dcc4a9
Sanket Mane <111991599+Sanketmane2323@users.noreply.github.com>
2023-10-09 01:37:12 +0530

    Footer  design pushed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import '../assets/styles/Footer.css';
+import React, { useEffect, useState } from "react";
+import "../assets/styles/Footer.css";
 
 const Footer = () => {
-
-  const[email, setEmail] = useState();
-  const emailChange= (event) => {
+  const [email, setEmail] = useState();
+  const emailChange = (event) => {
     setEmail(event.target.value);
-  }
+  };
 
   return (
     <>
-
       <footer className="footer">
+      
+          <p className="copyright-text">© 2023 Library -@All rigth reserved</p>
+          <p className="design-txt">Designed By Ajay Negi</p>
 
-        <div className='newsletter-container'>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
+
+//previous footer
+{
+  /* <div className='newsletter-container'>
           <form >
 
             <div className='newsletter-text'>
@@ -41,61 +51,5 @@ const Footer = () => {
 
           <p>&copy; { new Date().getFullYear() } </p>
           <p>Made with ❤️ by Ajay Negi</p>
-        </div>
-
-      </footer>
-
-
-
-
-
-
-
-
-
-
-
-
-{/* 
-
-      <footer className='custom-footer'>
-      <div className='container custom-container'>
-        <form onSubmit={handleSubscribe} className='custom-form'>
-          <div className='custom-row'>
-            <div className='custom-column custom-text'>
-              <p>
-                <strong>Sign up for our newsletter</strong>
-              </p>
-            </div>
-
-            <div className='custom-column'>
-              <input
-                type='text'
-                id='email'
-                className='custom-input'
-                placeholder='Email address'
-              />
-            </div>
-
-            <div className='custom-column'>
-              <button type='submit' className='custom-button'>Subscribe</button>
-            </div>
-          </div>
-        </form>
-      </div>
-
-      <div className='custom-footer-text'>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='custom-link' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>
-    </footer> */}
-
-
-    </>
-  )
+        </div> */
 }
-  
-export default Footer
-
