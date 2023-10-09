53b5b0dcf4111a0a81b3d65ed44f6a6cbfe75edb
Robin <robin.bruch@gmx.de>
2023-10-09 18:34:53 +0200

    Update package.json and removed Temporary-tag

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,11 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer';
-import BookList from "../components/BookList.jsx";
 import img2 from "../assets/images/books.jpg"
 import '../assets/styles/Contact.css';
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Temporary from "../components/Temporary.jsx";
 
 
 const Contact = () => {
@@ -41,16 +39,16 @@ const Contact = () => {
           <h2 className='form-headline'>Contact Us</h2>
           <form className='form-wrapper' ref={form} onSubmit={sendEmail}>
           <label htmlFor="user_name" className="label">Full Name:</label>
-            <input className="input" type='text' name='user_name' placeholder='Full Name' required/>
+            <input id="user_name" className="input" type='text' name='user_name' placeholder='Full Name' required/>
 
             <label htmlFor="user_email" className="label">Email:</label>
-            <input className="input" type='email' name='user_email' placeholder='Email' required/>
+            <input id="user_email" className="input" type='email' name='user_email' placeholder='Email' required/>
 
             <label htmlFor="user_subject" className="label">Subject:</label>
-            <input className="input" type='text' name='user_subject' placeholder='Subject' required/>
+            <input id="user_subject" className="input" type='text' name='user_subject' placeholder='Subject' required/>
 
             <label htmlFor="message" className="label">Message:</label>
-            <textarea name='message' cols="30" rows="10" placeholder='Message'></textarea>
+            <textarea id="message" name='message' cols="30" rows="10" placeholder='Message'></textarea>
 
             <button className="submit-btn" type='submit'>Send Message</button>
           </form>
@@ -58,7 +56,6 @@ const Contact = () => {
           <img className="contact-img" src={img2} alt='Picture of some books'/>
         </div>
       </div>
-      <Temporary/>
     <Footer/>
     </>
   )
