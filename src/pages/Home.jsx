a4a9320a9a2a9cba18c7cf8b0b8205d702cc386e
ajaynegi45 <ajaynegi3345@icloud.com>
2024-01-19 20:23:40 +0530

    Add Featured Books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer';
 import Money from '../assets/images/Money.jpg';
+import OOPS from "../assets/images/OOPS.png";
+import Spring from  "../assets/images/Spring.png";
+import Refactor from "../assets/images/Refactor.png";
+import Programmer from "../assets/images/Programmer.png";
 import HomeSearch from '../components/HomeSearch';
 import '../assets/styles/Home.css';
 
@@ -41,54 +45,44 @@ const Home = () => {
 
       <div className="card">
         <div className="img-box">
-          <img src={Money} alt="" />
+          <img src={OOPS} alt="" />
         </div>
         <div className="text-box">
-          <h2>The Psychology of Money</h2>
-          <span>Morgan Housel</span>
+          <h2>Object Oriented</h2>
+          <span>Matt Weisfeld</span>
         </div>
       </div>
 
 
       <div className="card">
         <div className="img-box">
-          <img src={Money} alt="" />
+          <img src={Spring} alt="" />
         </div>
         <div className="text-box">
-          <h2>The Psychology of Money</h2>
-          <span>Morgan Housel</span>
+          <h2>Spring In Action</h2>
+          <span>Manning</span>
         </div>
       </div>
 
 
       <div className="card">
         <div className="img-box">
-          <img src={Money} alt="" />
+          <img src={Programmer} alt="" />
         </div>
         <div className="text-box">
-          <h2>The Psychology of Money</h2>
-          <span>Morgan Housel</span>
+          <h2>The Pragmatic Programmer</h2>
+          <span>Person</span>
         </div>
       </div>
 
 
       <div className="card">
         <div className="img-box">
-          <img src={Money} alt="" />
+          <img src={Refactor} alt="" />
         </div>
         <div className="text-box">
-          <h2>The Psychology of Money</h2>
-          <span>Morgan Housel</span>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="img-box">
-          <img src={Money} alt="" />
-        </div>
-        <div className="text-box">
-          <h2>The Psychology of Money</h2>
-          <span>Morgan Housel</span>
+          <h2>Refactoring</h2>
+          <span>Kent Beck</span>
         </div>
       </div>
 
